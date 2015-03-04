e3fe2c9f6cabec7f05b4461bf4caa6e2621e74d0
ocefpaf <ocefpaf@gmail.com>
2015-03-04 08:29:28 -0300

    Adds `is_model()` several tests and some bug fixes

--- utilities/iris_utils.py
+++ utilities/iris_utils.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import division, absolute_import
 
 # Standard library.
 from datetime import datetime
@@ -19,19 +19,18 @@ iris.FUTURE.netcdf_promote = True
 iris.FUTURE.cell_datetime_objects = True
 
 
-__all__ = ['z_coord',
+__all__ = ['is_model',
+           'z_coord',
            'get_surface',
            'time_coord',
            'time_near',
            'time_slice',
-           'time_constraint',
            'bbox_extract_2Dcoords',
            'bbox_extract_1Dcoords',
            'subset',
            'get_cubes',
            'add_mesh',
            'standardize_fill_value',
-           'make_aux_coord',
            'ensure_timeseries',
            'add_station',
            'remove_ssh',
@@ -52,11 +51,14 @@ def _source_of_data(cube, coverage_content_type='modelResult'):
     Examples
     --------
     >>> import iris
+    >>> import warnings
     >>> iris.FUTURE.netcdf_promote = True
     >>> url = ("http://testbedapps-dev.sura.org/thredds/dodsC/"
     ...        "in/vims/selfe/ike/ultralite/vardrag/nowave/2d")
-    >>> cubes = iris.load_raw(url, 'sea_surface_height_above_geoid')
-    >>> [source_of_data(cube) for cube in cubes]
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cubes = iris.load_raw(url, 'sea_surface_height_above_geoid')
+    >>> [_source_of_data(cube) for cube in cubes]
     [True, True]
     """
 
@@ -68,29 +70,49 @@ def _source_of_data(cube, coverage_content_type='modelResult'):
 
 
 def is_model(cube):
-    """Heuristic way to find if a cube data is `modelResult` or not.
+    """
+    Heuristic way to find if a cube data is `modelResult` or not.
     WARNING: This function may return False positives and False
-    negatives!!!"""
+    negatives!!!
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> iris.FUTURE.netcdf_promote = True
+    >>> url = ("http://crow.marine.usf.edu:8080/thredds/dodsC/"
+    ...        "FVCOM-Nowcast-Agg.nc")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cubes = iris.load_raw(url, 'sea_surface_height_above_geoid')
+    >>> [is_model(cube) for cube in cubes]
+    [True]
+    >>> url = ("http://thredds.cdip.ucsd.edu/thredds/dodsC/cdip/archive/"
+    ...        "043p1/043p1_d17.nc")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cubes = iris.load_raw(url, 'sea_surface_temperature')
+    >>> [is_model(cube) for cube in cubes]
+    [False]
+
+    """
     # First criteria (Strong): "forecast" word in the time coord.
     try:
         coords = cube.coords(axis='T')
         for coord in coords:
             if 'forecast' in coord.name():
-                print("Time axis")
                 return True
     except CoordinateNotFoundError:
         pass
     # Second criteria (Strong): `UGRID` cubes are models.
     conventions = cube.attributes.get('Conventions', 'None')
-    if conventions.upper() == 'UGRID':
-        print("UGRID")
+    if 'UGRID' in conventions.upper():
         return True
     # Third criteria (Strong): dimensionless coords are present.
     try:
         coords = cube.coords(axis='Z')
         for coord in coords:
             if 'ocean_' in coord.name():
-                print("Vertical axis")
                 return True
     except CoordinateNotFoundError:
         pass
@@ -98,13 +120,27 @@ def is_model(cube):
     cdm_data_type = cube.attributes.get('cdm_data_type', 'None')
     feature_type = cube.attributes.get('featureType', 'None')
     if cdm_data_type.upper() == 'GRID' or feature_type.upper() == 'GRID':
-        print("GRID")
         return True
     return False
 
 
 def z_coord(cube):
-    """Heuristic way to return **one** the vertical coordinate."""
+    """
+    Heuristic way to return **one** the vertical coordinate.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> z_coord(cube).name()
+    u'S-coordinate at RHO-points'
+
+    """
     water_level = ['sea_surface_height',
                    'sea_surface_elevation',
                    'sea_surface_height_above_geoid',
@@ -122,8 +158,25 @@ def z_coord(cube):
 
 
 def get_surface(cube):
-    """Work around `iris.cube.Cube.slices` error:
-    The requested coordinates are not orthogonal."""
+    """
+    Work around `iris.cube.Cube.slices` error:
+    The requested coordinates are not orthogonal.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> cube.ndim == 4
+    True
+    >>> get_surface(cube).ndim == 3
+    True
+
+    """
     z = z_coord(cube)
     if z:
         positive = z.attributes.get('positive', None)
@@ -137,7 +190,22 @@ def get_surface(cube):
 
 
 def time_coord(cube):
-    """Return the variable attached to time axis and rename it to time."""
+    """
+    Return the variable attached to time axis and rename it to time.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> time_coord(cube).name()
+    u'time'
+
+    """
     try:
         cube.coord(axis='T').rename('time')
     except CoordinateNotFoundError:
@@ -147,7 +215,23 @@ def time_coord(cube):
 
 
 def time_near(cube, datetime):
-    """Return the nearest index to a `datetime`."""
+    """
+    Return the nearest index to a `datetime`.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> from datetime import datetime
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> isinstance(time_near(cube, datetime.utcnow()), int)
+    True
+
+    """
     timevar = time_coord(cube)
     try:
         time = timevar.units.date2num(datetime)
@@ -158,8 +242,26 @@ def time_near(cube, datetime):
 
 
 def time_slice(cube, start, stop=None):
-    """Slice time by indexes using a nearest criteria.
-    NOTE: Assumes time is the first dimension!"""
+    """
+    Slice time by indexes using a nearest criteria.
+    NOTE: Assumes time is the first dimension!
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> from datetime import datetime, timedelta
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> stop = datetime.utcnow()
+    >>> start = stop - timedelta(days=7)
+    >>> time_slice(cube, start, stop).shape[0] < cube.shape[0]
+    True
+
+    """
     istart = time_near(cube, start)
     if stop:
         istop = time_near(cube, stop)
@@ -172,22 +274,29 @@ def time_slice(cube, start, stop=None):
         return cube[istart, ...]
 
 
-def time_constraint(cube, start, stop):
-    """Slice time by constraint."""
-    def begin(cell):
-        return cell >= start
-
-    def end(cell):
-        return cell <= stop
-    constraint = Constraint(begin & end)
-    return cube.extract(constraint)
-
-
 def _minmax(v):
     return np.min(v), np.max(v)
 
 
 def _get_indices(cube, bbox):
+    """
+    Get the 4 corner indices of a `cube` given a `bbox`.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> bbox = [-87.40, 24.25, -74.70, 36.70]
+    >>> idxs = _get_indices(cube, bbox)
+    >>> [isinstance(idx, int) for idx in idxs]
+    [True, True, True, True]
+
+    """
     from oceans import wrap_lon180
     lons = cube.coord('longitude').points
     lats = cube.coord('latitude').points
@@ -204,15 +313,49 @@ def _get_indices(cube, bbox):
 
 
 def bbox_extract_2Dcoords(cube, bbox):
-    """Extract a sub-set of a cube inside a lon, lat bounding box
-    bbox=[lon_min lon_max lat_min lat_max].
+    """
+    Extract a sub-set of a cube inside a lon, lat bounding box
+    bbox = [lon_min lon_max lat_min lat_max].
     NOTE: This is a work around too subset an iris cube that has
-    2D lon, lat coords."""
+    2D lon, lat coords.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> bbox = [-87.40, 24.25, -74.70, 36.70]
+    >>> new_cube = bbox_extract_2Dcoords(cube, bbox)
+    >>> cube.shape != new_cube.shape
+    True
+
+    """
     imin, imax, jmin, jmax = _get_indices(cube, bbox)
     return cube[..., imin:imax, jmin:jmax]
 
 
 def bbox_extract_1Dcoords(cube, bbox):
+    """
+    Same as bbox_extract_2Dcoords but for 1D coords.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> url = "http://oos.soest.hawaii.edu/thredds/dodsC/pacioos/hycom/global"
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> bbox = [272.6, 24.25, 285.3, 36.70]
+    >>> new_cube = bbox_extract_1Dcoords(cube, bbox)
+    >>> cube.shape != new_cube.shape
+    True
+
+    """
     lat = Constraint(latitude=lambda cell: bbox[1] <= cell < bbox[3])
     lon = Constraint(longitude=lambda cell: bbox[0] <= cell <= bbox[2])
     cube = cube.extract(lon & lat)
@@ -220,9 +363,25 @@ def bbox_extract_1Dcoords(cube, bbox):
 
 
 def subset(cube, bbox):
-    """Sub sets cube with 1D or 2D lon, lat coords.
+    """
+    Subsets cube with 1D or 2D lon, lat coords.
     Using `intersection` instead of `extract` we deal with 0--360
-    longitudes automagically."""
+    longitudes automagically.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> url = "http://oos.soest.hawaii.edu/thredds/dodsC/pacioos/hycom/global"
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> bbox = [272.6, 24.25, 285.3, 36.70]
+    >>> new_cube = subset(cube, bbox)
+    >>> cube.shape != new_cube.shape
+    True
+
+    """
     if (cube.coord(axis='X').ndim == 1 and cube.coord(axis='Y').ndim == 1):
         # Workaround `cube.intersection` hanging up on FVCOM models.
         title = cube.attributes.get('title', None)
@@ -249,12 +408,36 @@ def filter_list(lista):
 
 def get_cubes(url, name_list, bbox=None, time=None, units=None, callback=None,
               constraint=None):
-    """Some station return more than one cube.  That is why I modified
-    `get_cube` to return a `Cubelist` instead.  Note that the cubes seems to be
-    duplicate sensors and/or sensors at different depths.
+    """
+    Return all cubes found using a `name_list` of standard_names and
+    constraining by `bbox`, `time`, and iris `constraint`.  The cubes found
+    can be transformed via a `callback` and the `units` can be converted.
+
     TODO: Create a criteria to choose a sensor.
     buoy = "http://129.252.139.124/thredds/dodsC/fldep.stlucieinlet..nc"
     buoy = "http://129.252.139.124/thredds/dodsC/lbhmc.cherrygrove.pier.nc"
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> from iris.unit import Unit
+    >>> from datetime import datetime, timedelta
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> stop = datetime.utcnow()
+    >>> start = stop - timedelta(days=7)
+    >>> bbox = [-87.40, 24.25, -74.70, 36.70]
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cubes = get_cubes(url, ['sea_water_potential_temperature'],
+    ...                       time=(start, stop), bbox=bbox,
+    ...                       units=Unit('Celsius'))
+    >>> isinstance(cubes, list)
+    True
+    >>> isinstance(cubes[0], iris.cube.Cube)
+    True
+
     """
 
     cubes = iris.load_raw(url, callback=callback)
@@ -293,7 +476,10 @@ def get_cubes(url, name_list, bbox=None, time=None, units=None, callback=None,
 
 
 def add_mesh(cube, url):
-    """Soon in an iris near you!"""
+    """
+    Adds the unstructured mesh info the to cube.  Soon in an iris near you!
+
+    """
     from pyugrid import UGrid
     ug = UGrid.from_ncfile(url)
     cube.mesh = ug
@@ -302,17 +488,22 @@ def add_mesh(cube, url):
 
 
 def standardize_fill_value(cube):
-    """Work around default `fill_value` when obtaining
+    """
+    Work around default `fill_value` when obtaining
     `_CubeSignature` (iris) using `lazy_data()` (biggus).
     Warning use only when you DO KNOW that the slices should
-    have the same `fill_value`!!!"""
+    have the same `fill_value`!!!
+
+    TODO: A fix was suggested to upstream (biggus) and this will
+    become obsolete.
+    """
     if ma.isMaskedArray(cube._my_data):
         fill_value = ma.empty(0, dtype=cube._my_data.dtype).fill_value
         cube._my_data.fill_value = fill_value
     return cube
 
 
-def make_aux_coord(cube, axis='Y'):
+def _make_aux_coord(cube, axis='Y'):
     """Make any given coordinate an Auxiliary Coordinate."""
     coord = cube.coord(axis=axis)
     cube.remove_coord(coord)
@@ -327,8 +518,8 @@ def ensure_timeseries(cube):
     """Ensure that the cube is CF-timeSeries compliant."""
     if not cube.coord('time').shape == cube.shape[0]:
         cube.transpose()
-    make_aux_coord(cube, axis='Y')
-    make_aux_coord(cube, axis='X')
+    _make_aux_coord(cube, axis='Y')
+    _make_aux_coord(cube, axis='X')
 
     cube.attributes.update({'featureType': 'timeSeries'})
     cube.coord("station name").attributes = dict(cf_role='timeseries_id')
@@ -347,7 +538,25 @@ def remove_ssh(cube):
     """
     Remove all `aux_coords` but time.  Should that has the same shape as
     the data.  NOTE: This also removes `aux_factories` to avoid update error
-    when removing the coordinate."""
+    when removing the coordinate.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> cube = get_surface(cube)
+    >>> len(cube.coords())
+    9
+    >>> cube = remove_ssh(cube)
+    >>> len(cube.coords())
+    8
+
+    """
     for factory in cube.aux_factories:
         cube.remove_aux_factory(factory)
     for coord in cube.aux_coords:
@@ -400,21 +609,63 @@ def save_timeseries(df, outfile, standard_name, **kw):
 
 
 def make_tree(cube):
-    """Create KDTree."""
+    """
+    Return a scipy KDTree object to search a cube.
+    NOTE: iris does have its own implementation for searching with KDTrees, but
+    it does not work for 2D coords like this one.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> from scipy.spatial import cKDTree as KDTree
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> cube = get_surface(cube)
+    >>> tree, lon, lat = make_tree(cube)
+    >>> isinstance(tree, KDTree)
+    True
+
+    """
     lon = cube.coord(axis='X').points
     lat = cube.coord(axis='Y').points
     # Structured models with 1D lon, lat.
     if (lon.ndim == 1) and (lat.ndim == 1) and (cube.ndim == 3):
         lon, lat = np.meshgrid(lon, lat)
-    # Unstructure are already paired!
+    # Unstructured are already paired!
     tree = KDTree(list(zip(lon.ravel(), lat.ravel())))
     return tree, lon, lat
 
 
 def get_nearest_water(cube, tree, xi, yi, k=10, max_dist=0.04, min_var=0.01):
-    """Find `k` nearest model data points from an iris `cube` at station
+    """
+    Find `k` nearest model data points from an iris `cube` at station
     lon: `xi`, lat: `yi` up to `max_dist` in degrees.  Must provide a Scipy's
-    KDTree `tree`."""
+    KDTree `tree`.
+
+    Examples
+    --------
+    >>> import iris
+    >>> import warnings
+    >>> from scipy.spatial import cKDTree as KDTree
+    >>> url = ("http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/"
+    ...        "SABGOM_Forecast_Model_Run_Collection_best.ncd")
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     cube = iris.load_cube(url, 'sea_water_potential_temperature')
+    >>> cube = get_surface(cube)
+    >>> tree, lon, lat = make_tree(cube)
+    >>> series, dist, idx = get_nearest_water(cube, tree,
+    ...                                       lon[0, 10], lat[0, 10],
+    ...                                        k=10, max_dist=0.04,
+    ...                                        min_var=0.01)
+    >>> idx == (0, 10)
+    True
+
+    """
     # TODO: Use rtree instead of KDTree.
     # NOTE: Based on the iris `_nearest_neighbour_indices_ndcoords`.
 
@@ -493,3 +744,8 @@ def var_lev_date(url=None, var=None, mytime=None, lev=0, subsample=1,
         sliced = sliced[lev, jmin:jmax:subsample, imin:imax:subsample]
     print('Slice retrieved in {:.2f} seconds'.format(time.time() - time0))
     return sliced
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
