429fbba7ae0cc77dfaa0681e35f8db16ce8a9daf
ocefpaf <ocefpaf@gmail.com>
2015-08-01 18:51:15 -0300

    filter_series

--- utilities/skill_score.py
+++ utilities/skill_score.py
@@ -1,6 +1,9 @@
 from __future__ import division, absolute_import
 
 import numpy as np
+import numpy.ma as ma
+from pandas import DataFrame
+from pandas.tseries.frequencies import to_offset
 
 try:
     from sklearn.metrics import (mean_absolute_error,
@@ -14,7 +17,13 @@ except ImportError:
 # TODO: Taylor, SST
 
 __all__ = ['both_valid',
-           'pearsonr_paired']
+           'pearsonr_paired',
+           'explained_variance_score',
+           'median_absolute_error',
+           'mean_absolute_error',
+           'r2_score',
+           'filter_series']
+
 
 def rmse(obs, model):
     """
@@ -31,6 +40,7 @@ def rmse(obs, model):
 
     """
     return np.sqrt(mean_squared_error(obs, model))
+
 
 def both_valid(x, y):
     """
@@ -69,6 +79,60 @@ def pearsonr_paired(x, y):
     x, y = x[mask], y[mask]
     r, p = pearsonr(x-x.mean(), y-y.mean())
     return r, p
+
+
+def filter_series(series, window_size=193, T=40, freq=None):
+    """
+    This function performs a lanczos filter on a pandas time-series and
+    returns the low and high pass data in a dataframe.
+
+    series : Pandas series
+    window_size : Size of the filter windows (default is 96+1+96).
+    T : Period of the filter. (The default of 40 hours filter should
+        get rid of all tides.)
+    freq : Pandas offset object or string for the resampling frequency
+          (default is no resampling).
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> import matplotlib.pyplot as plt
+    >>> from pandas import Series, date_range
+    >>> N = 700
+    >>> noise = np.random.randn(N)
+    >>> low_noise = 4 * np.sin(np.pi * np.arange(N) / 360.) + noise
+    >>> out = 2 * np.sin(2 * np.pi * np.arange(N) / 12.42) + low_noise
+    >>> t = date_range(start='1980-01-19', periods=N, freq='1D')
+    >>> out = Series(out, index=t)
+    >>> kw = dict(window_size=193, T=40)
+    >>> df = filter_series(out, **kw)
+    >>> fig, ax = plt.subplots(figsize=(9, 2.75))
+    >>> ax = df['low'].plot(ax=ax, label='low')
+    >>> ax = df['high'].plot(ax=ax, label='high')
+    >>> ax = out.plot(ax=ax, label='original')
+    >>> leg = ax.legend()
+
+    """
+    from oceans import lanc
+
+    T *= 60*60.  # To seconds.
+
+    if freq:
+        freq = to_offset(freq)
+        series = series.resample(freq)
+        T /= freq.delta.total_seconds()
+    freq = 1./T
+
+    series = series - series.mean()
+    mask = np.isnan(series)
+    series.interpolate(inplace=True)
+
+    wt = lanc(window_size, freq)
+    low = np.convolve(wt, series, mode='same')
+    low = ma.masked_array(low, mask)
+    high = series - low
+    data = dict(low=low, high=high)
+    return DataFrame(data, index=series.index)
 
 
 if __name__ == '__main__':
